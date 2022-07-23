dc5b6a652851212a321f1fb8754645b1cb8283cd
HadjSalemMohamedHedi <57866953+HadjSalemMohamedHedi@users.noreply.github.com>
2022-07-23 16:58:43 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { DevisComponent } from './devis/devis.component';
+import { HomeComponent } from './home/home.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { SecteurComponent } from './secteur/secteur.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"services",component:ServicesComponent},
+  {path:"projets",component:ProjetsComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"secteurs",component:SecteurComponent},
+  {path:"devis",component:DevisComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InViewportModule } from '@thisissoon/angular-inviewport';
+import { ScrollSpyModule } from '@thisissoon/angular-scrollspy';
+
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { ContactComponent } from './contact/contact.component';
+import { SecteurComponent } from './secteur/secteur.component';
+import { DevisComponent } from './devis/devis.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ServicesComponent,
+    ProjetsComponent,
+    ContactComponent,
+    SecteurComponent,
+    DevisComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
